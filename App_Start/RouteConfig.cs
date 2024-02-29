c61704c2fad15d392453e05a666cadababb2ef1b
mavyfaby <maverickfabroa@gmail.com>
2024-02-29 15:01:03 +0800

    chore(app): set home as default page and add icon

--- App_Start/RouteConfig.cs
+++ App_Start/RouteConfig.cs
@@ -13,6 +13,7 @@ namespace GraveGates
             var settings = new FriendlyUrlSettings();
             settings.AutoRedirectMode = RedirectMode.Permanent;
             routes.EnableFriendlyUrls(settings);
+            routes.MapPageRoute("", "", "~/Home.aspx");
         }
     }
 }
---
+++ gg.sql
@@ -0,0 +1,12 @@
+﻿CREATE DATABASE gg;
+
+use gg;
+
+CREATE TABLE users (
+	id INT NOT NULL PRIMARY KEY,
+	first_name VARCHAR(64) NOT NULL,
+	last_name VARCHAR(64) NOT NULL,
+	email VARCHAR(128) NOT NULL, 
+	password VARCHAR(128) NOT NULL,
+	date_stamp DATETIME NOT NULL
+);
